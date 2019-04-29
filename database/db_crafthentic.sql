74839a302529180db24b00fd0571a63e39e9136b
joshgal <joga3333.jg@gmail.com>
2019-04-29 21:06:30 +0700

    Update db_crafthentic.sql

--- database/db_crafthentic.sql
+++ database/db_crafthentic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2019 at 08:47 AM
+-- Generation Time: Apr 29, 2019 at 04:06 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -49,6 +49,31 @@ INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `harga_brg`, `deskripsi`, `katego
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_funding`
+--
+
+CREATE TABLE `tb_funding` (
+  `id_funding` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `id_umkm` int(11) NOT NULL,
+  `jumlah_funding` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_funding`
+--
+
+INSERT INTO `tb_funding` (`id_funding`, `id_pengguna`, `id_umkm`, `jumlah_funding`) VALUES
+(1, 6, 1, 1000000),
+(2, 3, 3, 2000),
+(3, 2, 2, 32300500),
+(4, 4, 3, 20000),
+(5, 4, 1, 350000),
+(6, 6, 1, 2000000);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_pengguna`
 --
 
@@ -83,15 +108,18 @@ CREATE TABLE `tb_umkm` (
   `nama_umkm` varchar(255) NOT NULL,
   `alamat_umkm` varchar(255) NOT NULL,
   `deskripsi_umkm` varchar(255) NOT NULL,
-  `foto_umkm` text NOT NULL
+  `foto_umkm` text NOT NULL,
+  `butuh_dana` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_umkm`
 --
 
-INSERT INTO `tb_umkm` (`id_umkm`, `nama_pemilik`, `nama_umkm`, `alamat_umkm`, `deskripsi_umkm`, `foto_umkm`) VALUES
-(1, 'Ahmad Damang', 'CV. Karya Ukir Maju', 'Jalan Kayu Manis RT 10 RW 08', 'Kami menjual berbagai ukiran kayu yang unik dengan harga yang sesuai dompet Anda', 'umkm1.jpg');
+INSERT INTO `tb_umkm` (`id_umkm`, `nama_pemilik`, `nama_umkm`, `alamat_umkm`, `deskripsi_umkm`, `foto_umkm`, `butuh_dana`) VALUES
+(1, 'Ahmad Damang', 'CV. Karya Ukir Maju', 'Jalan Kayu Manis RT 10 RW 08', 'Kami menjual berbagai ukiran kayu yang unik dengan harga yang sesuai dompet Anda. Kami jamin produk kami ada banyak.', 'umkm1.jpg', 38000000),
+(2, 'Ivan Nyoman Marbun', 'CV Nikmat Doldi', 'Jalan Badung Timur 304 Bali', 'Kami menjual berbagai macam kerajinan tangan berupa ukiran kayu yang unik dan mampu membuat Anda bahagia.', 'umkm2.jpg', 50000000),
+(3, 'Aldrin Liong', 'CV Liong Cipta Batik', 'Jalan Sukabirus No. 1 Jakarta Selatan', 'Liong Cipta Batik merupakan UMKM yang memproduksi batik khas dengan motif naga bonar, dijamin yang pakai langsung sipit.', 'umkm3.jpg', 200000000);
 
 --
 -- Indexes for dumped tables
@@ -103,6 +131,14 @@ INSERT INTO `tb_umkm` (`id_umkm`, `nama_pemilik`, `nama_umkm`, `alamat_umkm`, `d
 ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`id_brg`),
   ADD KEY `id_pengguna` (`id_pengguna`);
+
+--
+-- Indexes for table `tb_funding`
+--
+ALTER TABLE `tb_funding`
+  ADD PRIMARY KEY (`id_funding`),
+  ADD KEY `fk_funding_1` (`id_pengguna`),
+  ADD KEY `fk_funding_2` (`id_umkm`);
 
 --
 -- Indexes for table `tb_pengguna`
@@ -127,6 +163,12 @@ ALTER TABLE `tb_barang`
   MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
+-- AUTO_INCREMENT for table `tb_funding`
+--
+ALTER TABLE `tb_funding`
+  MODIFY `id_funding` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
@@ -136,7 +178,7 @@ ALTER TABLE `tb_pengguna`
 -- AUTO_INCREMENT for table `tb_umkm`
 --
 ALTER TABLE `tb_umkm`
-  MODIFY `id_umkm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_umkm` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -147,6 +189,13 @@ ALTER TABLE `tb_umkm`
 --
 ALTER TABLE `tb_barang`
   ADD CONSTRAINT `tb_barang_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `tb_pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tb_funding`
+--
+ALTER TABLE `tb_funding`
+  ADD CONSTRAINT `fk_funding_1` FOREIGN KEY (`id_pengguna`) REFERENCES `tb_pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_funding_2` FOREIGN KEY (`id_umkm`) REFERENCES `tb_umkm` (`id_umkm`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
