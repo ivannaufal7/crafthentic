a990f17ff6ab603ec487f7e26e33240be5195f6f
joshgal <joga3333.jg@gmail.com>
2019-04-29 21:05:30 +0700

    aing aral(view form_pendanaan, model umkm, controller umkm_control)

--- application/controllers/umkm_control.php
+++ application/controllers/umkm_control.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class umkm_control extends CI_Controller {
+
 	function __construct(){
          parent::__construct();
          $this->load->library(array('form_validation'));
@@ -9,8 +10,18 @@ class umkm_control extends CI_Controller {
          $this->load->model('M_umkm');
     }
 
-    function get_all_umkm(){
-    	$data = $this->M_umkm->get_umkm_data();
+    function detil_umkm($id){
+    	$data['umkm'] = $this->M_umkm->get_specified_umkm($id);
+    	$this->load->view('form_pendanaan', $data);
     }
 
+    function taruh_dana($id){
+    	$data = array(
+            'id_pengguna' => $this->input->post('id'),
+            'id_umkm' => $id,
+            'jumlah_funding'=> $this->input->post('jumlah_funding')
+        );
+        $this->M_umkm->insert_data_dana($data);
+    	redirect('home');
+    }
 }
